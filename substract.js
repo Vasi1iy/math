6071cd43c1555f9e20d70cc4d8da83c7543cdff7
Vasiliy Shvetsov <92184647+Vasi1iy@users.noreply.github.com>
2022-01-02 18:18:20 +0200

    Add files via upload

---
+++ substract.js
@@ -0,0 +1,42 @@
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+
+const audio = document.getElementById("myAudio");
+
+let answer = 0;
+
+function generate_equation(){
+    let num1 = Math.floor(Math.random() * 999);
+    let num2 = Math.floor(Math.random() * 999);
+    let dummyAnswer1 = Math.floor(Math.random() * 999);
+    let dummyAnswer2 = Math.floor(Math.random() * 999);
+
+    let allAnswers = [];
+    let switchAnswers = [];
+
+    answer = num1 - num2;
+
+    document.getElementById("num1").innerHTML = num1;
+    document.getElementById("num2").innerHTML = num2;
+
+    allAnswers = [answer, dummyAnswer1, dummyAnswer2];
+
+    for(i= allAnswers.length; i--;){
+        switchAnswers.push(allAnswers.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
+    };
+
+    option1.innerHTML = switchAnswers[0];
+    option2.innerHTML = switchAnswers[1];
+    option3.innerHTML = switchAnswers[2];
+};
+
+const answerAction = (e) => {
+    e.target.innerHTML == answer ? generate_equation() : audio.play()
+}
+
+option1.addEventListener("click", answerAction);
+option2.addEventListener("click", answerAction);
+option3.addEventListener("click", answerAction);
+
+generate_equation();
